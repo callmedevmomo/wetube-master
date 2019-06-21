e9a752418809bbdae3b77285ecb90dae1f61ec43
momo <callmedevmomo@gmail.com>
2019-06-21 19:44:24 +0900

    before View

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+const app = express();
+
+// const betweenHome = (req, res, next) => {
+//   console.log("I'm between ");
+//   next();
+// };
+
+// app.use(betweenHome);
+
+app.use(cookieParser());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
