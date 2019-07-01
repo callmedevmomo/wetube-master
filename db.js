3f014222753831ec37c8c30bd58f7f47ceda47b1
momo <callmedevmomo@gmail.com>
2019-07-01 15:59:41 +0900

    mognoose set

--- db.js
+++ db.js
@@ -1,54 +1,15 @@
-export const videos = [
-  {
-    id: 213214213,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123124,
-      name: "momo",
-      email: "momo@dev.com"
-    }
-  },
-  {
-    id: 42144124,
-    title: "Super awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123124,
-      name: "momo",
-      email: "momo@dev.com"
-    }
-  },
-  {
-    id: 1231251243,
-    title: "Beautiful awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123124,
-      name: "momo",
-      email: "momo@dev.com"
-    }
-  },
-  {
-    id: 12400230,
-    title: "Text awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123124,
-      name: "momo",
-      email: "momo@dev.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB connection:${error}`);
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,9 +1,15 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+import "./models/Video";
+import "./models/Comment";
 
-const PORT = 4000;
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const handleListening = () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`✅ Listening on http://localhost:${PORT}`);
 };
 
 app.listen(PORT, handleListening);
